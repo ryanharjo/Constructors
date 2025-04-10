8cd4956f59f30ba19c413520e547158ff1bfc8ad
Ryan Harjo <ryanharjo01@gmail.com>
2025-04-10 00:40:16 -0500

    Add files via upload

---
+++ Constructors_Harjo_Ryan/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Constructors_Harjo_Ryan
+{
+    internal class Game
+    {
+        public string Title; // Public string field to store the game's title
+        public string Genre; // Public string field to store the game's genre
+                            
+        // Constructor for the 'Game' class with default parameters
+        // If no argument is provided, it will default to "Unknown Title" and "Genre"
+        public Game(string titleParam = "Unknown Title", string genreParam = "Genre") 
+        {
+            Title = titleParam; // Assign the value of titleParam to the class's title field
+            Genre = genreParam; // Assign the value of genreParam to the class's genre field
+        }
+    }
+}
---
+++ Constructors_Harjo_Ryan/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Constructors_Harjo_Ryan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate a new Game object with specified title and genre
+            Game myGame = new Game("Assassin's Creed Odyssey", "Action-Adventure Role-Playing Game");
+            // Print out the title and genre of the 'myGame' object to the console                                                                           
+            Console.WriteLine($"Game Title: {myGame.Title} and it is an {myGame.Genre}");
+
+                                                                                         
+
+            // Create another instance of the Game class with different values for title and genre
+            Game myGame2 = new Game("God of War Ragnarök", "Third-Person Action-Adventure Game");
+            // Print out the title and genre of the 'myGame2' object to the console
+            Console.WriteLine($"Game Title: {myGame2.Title} and it is a {myGame2.Genre}");
+
+
+        }
+    }
+}
